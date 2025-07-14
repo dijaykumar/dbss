233ea3d821a7a905894a22d2c5bc96c739516cb9
dijaykumar <dijaykumar@gmail.com>
2025-07-14 05:23:14 +0000

    Edited

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import joblib
 
 app = Flask(__name__)
 
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
